b6ae0ccb65bfa43cc60d3d030103a974be9520a5
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-09-22 12:51:36 +0530

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -17,3 +17,9 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
+# loading the dataset:
+train_dataset = datasets.CIFAR10(root='./data',\
+              train=True, download=True, transform=transform)
+dataloader = torch.utils.data.DataLoader(train_dataset, \
+                                batch_size=32, shuffle=True)
+
