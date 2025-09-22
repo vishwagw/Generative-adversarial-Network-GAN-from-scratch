b9a2f19ae19bf73f2bcd95f173cb672a5c317826
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-09-22 12:51:12 +0530

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -1,1 +1,12 @@
 # creating the GAN:
+# importing the libraries:
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+# deciding the device to run the model on:
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
