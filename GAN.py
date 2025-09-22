e7656ff78fc56c051aa008b6ff1f1ceab40423d0
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-09-22 12:52:20 +0530

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -30,4 +30,72 @@ beta1 = 0.5
 beta2 = 0.999
 num_epochs = 10
 
+# defining the generator model:
+class Generator(nn.Module):
+    def __init__(self, latent_dim):
+        super(Generator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(latent_dim, 128 * 8 * 8),
+            nn.ReLU(),
+            nn.Unflatten(1, (128, 8, 8)),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128, momentum=0.78),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64, momentum=0.78),
+            nn.ReLU(),
+            nn.Conv2d(64, 3, kernel_size=3, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        return img
+    
+# defining the discriminator model:
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+        nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.Dropout(0.25),
+        nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+        nn.ZeroPad2d((0, 1, 0, 1)),
+        nn.BatchNorm2d(64, momentum=0.82),
+        nn.LeakyReLU(0.25),
+        nn.Dropout(0.25),
+        nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+        nn.BatchNorm2d(128, momentum=0.82),
+        nn.LeakyReLU(0.2),
+        nn.Dropout(0.25),
+        nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(256, momentum=0.8),
+        nn.LeakyReLU(0.25),
+        nn.Dropout(0.25),
+        nn.Flatten(),
+        nn.Linear(256 * 5 * 5, 1),
+        nn.Sigmoid()
+    )
+
+    def forward(self, img):
+        validity = self.model(img)
+        return validity
+    
+# initializing the models:
+generator = Generator(latent_dim).to(device)
+discriminator = Discriminator().to(device)
+
+adversarial_loss = nn.BCELoss()
+
+optimizer_G = optim.Adam(generator.parameters()\
+                         , lr=lr, betas=(beta1, beta2))
+optimizer_D = optim.Adam(discriminator.parameters()\
+                         , lr=lr, betas=(beta1, beta2))
+
+
 
